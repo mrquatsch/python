882f6f861b80bfb0f3e726342a9bd2b612cfc7f6
mrquatsch@gmail.com <mrquatsch@gmail.com>
2014-03-19 07:16:35 -0500

    updated euler3

--- ProjectEuler/euler3.py
+++ ProjectEuler/euler3.py
@@ -1,4 +1,17 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
+number = 13195
+largestPrime = 0
+prime = 0
 
+for x in range(1,number):
+	for y in range(2, x - 1):
+		prime = 0
+		if x % y == 0:
+			prime = 0
+		else:
+			prime = 1
+	if prime == 1:
+		largestPrime = x
+print str(largestPrime)
